10774573788bf9d8843afa5f667e850b5c3dc79a
onthecrow <onthecrow@doubletapp.io>
2024-11-08 23:07:32 +0000

    Small fixes

--- app/src/main/java/com/onthecrow/db_playground/MainActivity.kt
+++ app/src/main/java/com/onthecrow/db_playground/MainActivity.kt
@@ -6,14 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -22,7 +31,6 @@ import com.onthecrow.db_playground.data.DatabaseManager
 import com.onthecrow.db_playground.data.SampleRepository
 import com.onthecrow.db_playground.ui.list.ListItem
 import com.onthecrow.db_playground.ui.theme.DbplaygroundTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -35,19 +43,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             DbplaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val isLoaded = databaseManager.prepopulatedState.collectAsState()
                     val dataState = sampleRepository.getSampleData().collectAsLazyPagingItems()
                     val listState = rememberLazyListState()
 
                     LaunchedEffect(Unit) {
-                        databaseManager.setRangeProvider { listState.firstVisibleItemIndex .. listState.firstVisibleItemIndex + listState.layoutInfo.visibleItemsInfo.size }
-                        launch {
-                            repeat(Int.MAX_VALUE) {
-                                listState.animateScrollBy(1000f, animationSpec = tween(1000, easing = LinearEasing))
-                            }
+                        databaseManager.setRangeProvider { listState.firstVisibleItemIndex..listState.firstVisibleItemIndex + listState.layoutInfo.visibleItemsInfo.size }
+                    }
+                    LaunchedEffect(dataState) {
+                        repeat(Int.MAX_VALUE) {
+                            listState.animateScrollBy(
+                                1000f,
+                                animationSpec = tween(1000, easing = LinearEasing)
+                            )
                         }
                     }
                     LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background),
                         state = listState,
                         contentPadding = PaddingValues(8.dp),
                         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -59,6 +73,17 @@ class MainActivity : ComponentActivity() {
                             dataState[index]?.let { repo ->
                                 ListItem(repo)
                             }
+                        }
+                    }
+                    if (isLoaded.value.not()) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator()
+                            Spacer(Modifier.size(8.dp))
+                            Text("Prepopulating DB...")
                         }
                     }
                 }
--- app/src/main/java/com/onthecrow/db_playground/data/DatabaseManager.kt
+++ app/src/main/java/com/onthecrow/db_playground/data/DatabaseManager.kt
@@ -1,16 +1,19 @@
 package com.onthecrow.db_playground.data
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
 import androidx.room.withTransaction
 import com.onthecrow.db_playground.ColorUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import java.util.LinkedList
 import kotlin.math.roundToInt
 
@@ -26,6 +29,7 @@ class DatabaseManager(
     private val changes = LinkedList<suspend () -> Unit>()
     private val mutex = Mutex()
 
+    val prepopulatedState = MutableStateFlow(false)
 
     init {
         MainScope().launch(Dispatchers.IO) {
@@ -62,7 +66,10 @@ class DatabaseManager(
      * These entities are then inserted into the database using the UserDao.
      */
     private suspend fun prepopulateDb() {
-        if (db.userDao().getOne() != null) return
+        if (db.userDao().getOne() != null) {
+            prepopulatedState.emit(true)
+            return
+        }
         val currentDate = System.currentTimeMillis()
         val entities = Array(1_000_000) {
             SampleEntity(
@@ -75,6 +82,7 @@ class DatabaseManager(
             )
         }
         db.userDao().insertAll(*entities)
+        prepopulatedState.emit(true)
     }
 
 
@@ -90,16 +98,21 @@ class DatabaseManager(
      * @note This function runs indefinitely in the background and should be started only once.
      */
     private fun runSyncJob() {
-        MainScope().launch(Dispatchers.IO) {
+        Log.d("DatabaseManager", "runSyncJob")
+        MainScope().launch(Dispatchers.Default) {
             while (true) {
                 delay(100)
                 mutex.withLock {
+                    Log.d("DatabaseManager", "Before changes")
                     db.withTransaction {
-                        changes.map { lambda ->
-                            launch { lambda() }
-                        }.joinAll()
+                        withContext(Dispatchers.IO) {
+                            changes.map { lambda ->
+                                launch { lambda() }
+                            }.joinAll()
+                        }
                     }
                     changes.clear()
+                    Log.d("DatabaseManager", "Changes processed")
                 }
             }
         }
@@ -160,7 +173,6 @@ class DatabaseManager(
      *  - The second parameter is the repeat interval in milliseconds.
      */
     private fun runDbModifications() {
-
         startPeriodicChange(10, 100) {
             val entity = db.userDao().getOne((1_000_000 * Math.random()).roundToInt() - 1)
             entity?.run { db.userDao().update(copy(color = ColorUtils.getRandomColor())) }
@@ -170,9 +182,9 @@ class DatabaseManager(
             entity?.run { db.userDao().update(copy(isRead = this.isRead.not())) }
         }
         startPeriodicChange(10, 100) {
-            val entity =
-                db.userDao().getOne((1_000_000 * Math.random()).roundToInt() - 1)
-            db.userDao().delete(entity!!)
+            db.userDao().getOne((1_000_000 * Math.random()).roundToInt() - 1)?.let { entity ->
+                db.userDao().delete(entity)
+            }
         }
         startPeriodicChange(10, 100) {
             db.userDao().insertAll(
